02722b7c8ef6ba5352d3697a46a07c087f891acb
laptop <clerc.fabien@bluewin.ch>
2021-06-24 11:10:37 +0200

    change API url

--- frontend/src/API/index.js
+++ frontend/src/API/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "https://motion.propulsion-home.ch/backend/api/";
+const baseURL = "https://teamonemotion.propulsion-learn.ch/backend/api/";
 
 const Axios = axios.create({
   baseURL: baseURL,
